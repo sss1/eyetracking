8efb385a588a2d657ebbfc0469736e52832bbe56
sss1 <sss1@andrew.cmu.edu>
2017-08-25 12:58:47 -0400

    Minor style improvements.

--- aggregating.py
+++ aggregating.py
@@ -10,22 +10,19 @@ import actual_scoring_copy as asc
 trendline_list_0dis = []
 trendline_list_same = []
 trendline_list_diff = []
-# subject_id_list = ["Anna","Clara","Eden","Hyunji","Kristen","Nick","Rebeka","Sara"]
-subject_id_list = ["A232","A233"]#,"A239","A240","A241","A242","A243","A244","A245","A247"]
-# subject_id_list = ["A197","A250","A252","A256","A257"]
-# maindir = "adult_pilot/"
-maindir = "3yo/"
-# maindir = "blinky_pilot/"
-filter_threshold = 0.75
+maindir = "adult_pilot/"; subject_id_list = ["Anna"]#,"Clara","Eden","Hyunji","Kristen","Nick","Rebeka","Sara"]
+# maindir = "3yo/"; subject_id_list = ["A232","A233","A239","A240","A241","A242","A243","A244","A245","A247"]
+# maindir = "blinky_pilot/"; subject_id_list = ["A197","A250","A252","A256","A257"]
+filter_threshold = 1 # between 0 and 1; higher value keeps more data
 
-for i in subject_id_list:
-    new_trendline = asc.read_and_analyze_file(maindir,"AllSame", i, filter_threshold = filter_threshold)
+for subject_id in subject_id_list:
+    new_trendline = asc.read_and_analyze_file(maindir,"AllSame", subject_id, filter_threshold = filter_threshold)
     if new_trendline != None and len(new_trendline) > 0:
         trendline_list_same.append(new_trendline)
-    new_trendline = asc.read_and_analyze_file(maindir,"AllDiff", i, filter_threshold = filter_threshold)
+    new_trendline = asc.read_and_analyze_file(maindir,"AllDiff", subject_id, filter_threshold = filter_threshold)
     if new_trendline != None and len(new_trendline) > 0:
         trendline_list_diff.append(new_trendline)
-    new_trendline = asc.read_and_analyze_file(maindir,"0Distractors", i, filter_threshold = filter_threshold)
+    new_trendline = asc.read_and_analyze_file(maindir,"0Distractors", subject_id, filter_threshold = filter_threshold)
     if new_trendline != None and len(new_trendline) > 0:
         trendline_list_0dis.append(new_trendline)
 
@@ -37,7 +34,7 @@ def plot_all_trials_and_mean(trendline_list):
   print 'np.shape(trendline_list): ' + str(np.shape(trendline_list))
   for trendline in trendline_list:
     plt.plot(trendline, color = 'blue', alpha = 0.4)
-  avg = map(np.mean,zip(*trendline_list))
+  avg = map(np.nanmean,zip(*trendline_list))
   plt.plot(range(len(avg)), avg, color = 'red', linewidth = 2)
 
 plt.subplot(2,3,1)
@@ -57,6 +54,10 @@ plot_all_trials(trendline_list_diff)
 plt.title('All Diff')
 # plt.xlabel('Frames')
 plt.ylabel('Pixels')
+
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles, labels)
+plt.legend(subject_id_list)
 
 plt.subplot(2,3,4)
 plot_all_trials_and_mean(trendline_list_0dis)
