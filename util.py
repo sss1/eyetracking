d0bd3fd5d60043b5b28101908163c25ed6b37581
sss1 <sss1@andrew.cmu.edu>
2017-12-06 13:54:05 -0500

    Implemented data imputation for small amounts of missing data.

--- util.py
+++ util.py
@@ -5,6 +5,63 @@ import numpy as np
 def error_fcn(et_x, et_y, trackit_x, trackit_y):
     return math.sqrt((et_x - trackit_x)**2 + (et_y - trackit_y)**2)
 
-def interpolate_to_length(X, new_len):
+# Given a D-dimensional sequence X of numbers, performs interpolate_to_length (independently) on each dimension of X
+# X is D x N, where D is the dimensionality and N is the sample length
+def interpolate_to_length_D(X, new_len):
+  D = X.shape[0]
+  X_new = np.zeros((D, new_len))
+  for d in range(D):
+    X_new[d, :] = __interpolate_to_length(X[d, :], new_len)
+  return X_new
+
+# Given a sequence X of numbers, returns the length-new_len linear interpolant of X
+def __interpolate_to_length(X, new_len):
   old_len = X.shape[0]
   return np.interp([(float(n)*old_len)/new_len for n in range(new_len)], range(old_len), X)
+
+# Given a D-dimensional sequence X of numbers, performs impute_missing_data_D (independently) on each dimension of X
+# X is D x N, where D is the dimensionality and N is the sample length
+def impute_missing_data_D(X, max_len = 10):
+  D = X.shape[0]
+  for d in range(D):
+    X[d, :] = __impute_missing_data(X[d, :], max_len)
+  return X
+
+# Given a sequence X of floats, replaces short streches (up to length max_len) of NaNs with linear interpolation
+#
+# For example, if
+#
+# X = np.array([1, NaN, NaN,  4, NaN,  6])
+#
+# then
+#
+# impute_missing_data(X, max_len = 1) == np.array([1, NaN, NaN, 5, 6])
+#
+# and
+#
+# impute_missing_data(X, max_len = 2) == np.array([1, 2, 3, 4, 5, 6])
+def __impute_missing_data(X, max_len):
+  last_valid_idx = -1
+  last_valid_val = float('nan')
+  for n in range(len(X)):
+    if not math.isnan(X[n]):
+      if last_valid_idx < n - 1: # there is missing data
+        if n - (max_len + 1) <= last_valid_idx: # amount of missing data is at most than max_len
+          first_last = np.array([X[last_valid_idx], X[n]]) # initial and final values from which to linearly interpolate
+          new_len = n - last_valid_idx + 1
+          X[last_valid_idx:(n + 1)] = np.interp([float(x)/(new_len - 1) for x in range(new_len)], [0, 1], first_last)
+          # print('Successfully interpolated a block of ' + str(new_len - 2) + ' NaN(s).')
+        else:
+          new_len = n - last_valid_idx + 1
+          print('Failed to interpolate a block of ' + str(new_len - 2) + ' NaN(s).')
+      last_valid_idx = n
+      last_valid_val = X[n]
+  return X
+
+# X = np.array([1, 2, 3])
+# print __impute_missing_data(X, 10)
+# X = np.array([0, 1])
+# print __interpolate_fixed_length(X, 5)
+# X = np.array([1, float('nan'), float('nan'), float('nan'), 5, float('nan'), float('nan'), 8])
+# print X
+# print __impute_missing_data(X, 3)
--- video_plotting.py
+++ video_plotting.py
@@ -1,21 +1,19 @@
-import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import time
-import sys
 from load_data import load_full_subject_data
-from util import error_fcn
-from eyetracking_hmm import getMLE
-from util import interpolate_to_length
+from eyetracking_hmm import getTrackItMLE
+from util import interpolate_to_length_D, impute_missing_data_D
 
-root = "/home/painkiller/Desktop/academic/projects/trackit/eyetracking/" # Laptop
+root = "/home/sss1/Desktop/projects/eyetracking/data/" # Office desktop
+# root = "/home/painkiller/Desktop/academic/projects/trackit/eyetracking/" # Laptop
 # root = "/home/sss1/Desktop/academic/projects/eyetracking/" # Home desktop
 subject_type = "adult_pilot/" # "3yo/" 
-TI_data_dir = "TrackItOutput/AllDiff/"
-ET_data_dir = "EyeTracker/AllDiff/"
-TI_fname = "shashank.csv" # "AnnaSame.csv" # "A232Same.csv" 
-ET_fname = "shashank1_12_5_2017_13_41.csv" # "AnnaSame_9_13_2016_13_25.csv" # "A232Same_3_29_2016_10_26.csv" 
+TI_data_dir = "TrackItOutput/AllSame/"
+ET_data_dir = "EyeTracker/AllSame/"
+TI_fname = "AnnaSame.csv" # "shashank.csv" # "A232Same.csv" 
+ET_fname = "AnnaSame_9_13_2016_13_25.csv" # "shashank1_12_5_2017_13_41.csv" # "A232Same_3_29_2016_10_26.csv" 
 
 TI_file_path = root + subject_type + TI_data_dir + TI_fname
 ET_file_path = root + subject_type + ET_data_dir + ET_fname
@@ -30,48 +28,38 @@ track_it_xy_list, distractors_xy_list, eye_track_xy_list \
 # boundaries of track-it grid
 x_min = 400
 x_max = 1600
-y_min = 000
+y_min = 0
 y_max = 1000
 
 space = 50 # number of extra pixels to display on either side of the plot
 
 # First set up the figure, the axis, and the plot element we want to animate
-lag = 10 # plot a time window of length lag, so we can see the trajectory
-print 'Number of trials: ' + str(len(track_it_xy_list))
+lag = 10 # plot a time window of length lag, so we can see the trajectory # added 1 and 2
+trials_to_show = range(len(track_it_xy_list))
+print 'Number of trials: ' + str(len(trials_to_show))
 
-# gbbgbgg
-# for trial_idx in range(len(track_it_xy_list)):
-for trial_idx in [0,3,5,6]:#range(1):
+for trial_idx in trials_to_show:
 
   # x and y lists for the current trial
   # Recall that eyetracking (60 Hz) uses twice the frequency of TrackIt (30 Hz)
   # We adjust for this by interpolating TrackIt to have the same number of
   # frames as eyetracking
   N = len(eye_track_xy_list[trial_idx][0]) # Number of eye-tracking frames in trial
-  N_short = 700 # TODO: REMOVE; TEMP DEBUGGING VARIABLE TO AVOID NANS
-  eye_track = np.array(eye_track_xy_list[trial_idx])[:, 0:N_short]
-  target = np.zeros((2, N_short))
-  target[0,:] = interpolate_to_length(np.array(track_it_xy_list[trial_idx][0]), N)[0:N_short]
-  target[1,:] = interpolate_to_length(np.array(track_it_xy_list[trial_idx][1]), N)[0:N_short]
+  eye_track = impute_missing_data_D(np.array(eye_track_xy_list[trial_idx]))[:, 0:N]
+  target = interpolate_to_length_D(np.array(track_it_xy_list[trial_idx]), N)[:,0:N]
   distractors_old = np.array(distractors_xy_list[trial_idx])
   distractors_old = distractors_old
-  distractors = np.zeros(distractors_old.shape[0:2] + (N_short,))
+  distractors = np.zeros(distractors_old.shape[0:2] + (N,))
   for k in range(distractors.shape[0]):
-    distractors[k,0,:] = interpolate_to_length(np.array(distractors_old[k,0,:]), N)[0:N_short]
-    distractors[k,1,:] = interpolate_to_length(np.array(distractors_old[k,1,:]), N)[0:N_short]
+    distractors[k,:,:] = interpolate_to_length_D(np.array(distractors_old[k,:,:]), N)[:,0:N]
 
-  MLE, X, mu = getMLE(eye_track, target, distractors)
+  MLE, X, mu = getTrackItMLE(eye_track, target, distractors)
   print MLE
-
-
-  # if not np.allclose(X.transpose(), eye_track):
-  #   raise ValueError
-  if not np.allclose(mu[1:mu.shape[0], :, :].swapaxes(1,2), distractors):
-    raise ValueError
 
   trial_length = target.shape[1]
   print 'Plotting trial ' + str(trial_idx) + ' with length ' + str(trial_length) + '.'
-
+ 
+  # initializate plot background and objects to plot
   fig = plt.figure()
   ax = plt.axes(xlim=(x_min, x_max), ylim = (y_min, y_max))
   trackit_line, = ax.plot([], [], lw = 2)
@@ -80,17 +68,9 @@ for trial_idx in [0,3,5,6]:#range(1):
   for j in range(len(distractors)):
     distractors_lines.extend(ax.plot([], [], 'r', lw = 2))
   state_point = ax.scatter([], [], s = 50)
-  
-  # initialization function: plot the background of each frame
-  def init():
-    # plt.axes(xlim=(x_min, x_max),ylim=(y_min, y_max))
-    # trackit_line.set_data([], [])
-    # eye_track_line.set_data([], [])
-    # for j in range(len(distractors)):
-    #   distractors_lines[j].set_data([], [])
-    return# trackit_line, eye_track_line,
 
-  # show frames in range(trial_length - lag)
+  # Rather than a single point, show tail of object trajectories (frames in range(trial_length - lag))
+  # This makes it much easier to follow objects visually
   def animate(i):
     trackit_line.set_data(target[0,i:(i + lag)], target[1,i:(i + lag)])
     eye_track_line.set_data(eye_track[0,i:(i + lag)], eye_track[1,i:(i + lag)])
@@ -106,13 +86,11 @@ for trial_idx in [0,3,5,6]:#range(1):
     plt.xlim([x_min, x_max])
     plt.ylim([y_min, y_max])
     timestep = 0.0333 / 2
-    time.sleep(timestep)
-    # print 'Frame: ' + str(i) + ' Error: ' + str(error_fcn(eye_track_xs[2*i], eye_track_ys[2*i], track_it_xs[i], track_it_ys[i]))
+    # time.sleep(timestep)
     return trackit_line, eye_track_line,
 
 
   anim = animation.FuncAnimation(fig, animate,
-                                 init_func = init,
                                  frames = trial_length - lag,
                                  interval = 20,
                                  blit = False,
