453bf53557fee8f4930cf77291a68fcad526f7ae
sss1 <sss1@andrew.cmu.edu>
2017-12-08 12:28:08 -0500

    Added file list.

---
+++ data_paths.py
@@ -0,0 +1,143 @@
+trackit_fnames_adult_0dis = ["Anna0dis.csv", \
+                             "Cheul0dis.csv", \
+                             "Clara0dis.csv", \
+                             "Eden0die.csv", \
+                             "Gabi0dis.csv", \
+                             "Hyunji0dis.csv", \
+                             "Kristen0dis.csv", \
+                             "Nick0dis.csv", \
+                             "Rebeka_0dis.csv", \
+                             "Sara_0dis.csv"]
+
+eyetrack_fnames_adult_0dis = ["Anna0dis_9_13_2016_13_29.csv", \
+                              "Cheul0dis_9_13_2016_13_0.csv", \
+                              "Clara0dis_9_13_2016_12_49.csv", \
+                              "Eden0dis_9_13_2016_12_32.csv", \
+                              "Gabi0dis_9_15_2016_11_54.csv", \
+                              "Hyunji0dis_9_13_2016_12_24.csv", \
+                              "Kristen0dis_9_13_2016_13_17.csv", \
+                              "Nick0dis_9_15_2016_12_8.csv", \
+                              "Rebeka_0dis_9_9_2016_12_24.csv", \
+                              "Sara_0dis_9_9_2016_12_9.csv"]
+
+trackit_fnames_adult_same = ["AnnaSame.csv", \
+                             "CheulSame.csv", \
+                             "ClaraSame.csv", \
+                             "EdenSame.csv", \
+                             "GabiSame.csv", \
+                             "HyunjiSame.csv", \
+                             "KristenSame.csv", \
+                             "NickSame.csv", \
+                             "Rebeka_Same.csv", \
+                             "Sara_Same.csv"]
+
+trackit_fnames_adult_same = ["AnnaSame_9_13_2016_13_25.csv", \
+                             "CheulSame_9_13_2016_13_3.csv", \
+                             "ClaraSame_9_13_2016_12_46.csv", \
+                             "EdenSame_9_13_2016_12_38.csv", \
+                             "GabiSame_9_15_2016_11_57.csv", \
+                             "HyunjiSame_9_13_2016_12_27.csv", \
+                             "KristenSame_9_13_2016_13_11.csv", \
+                             "NickSame_9_15_2016_12_3.csv", \
+                             "Rebeka_Same_9_9_2016_12_27.csv", \
+                             "Sara_AllSame_9_9_2016_12_12.csv"]
+
+trackit_fnames_adult_diff = ["AnnaDiff.csv", \
+                             "CheulDiff.csv", \
+                             "ClaraDiff.csv", \
+                             "EdenDiff.csv", \
+                             "GabiDiff.csv", \
+                             "HyunjiDiff.csv", \
+                             "KristenDiff.csv", \
+                             "NickDiff.csv", \
+                             "Rebeka_Diff.csv", \
+                             "Sara_Diff.csv"]
+
+eyetrack_fnames_adult_diff = ["AnnaDiff_9_13_2016_13_27.csv", \
+                              "CheulDiff_9_13_2016_13_6.csv", \
+                              "Claradiff_9_13_2016_12_43.csv", \
+                              "Edendiff_9_13_2016_12_35.csv", \
+                              "GabiDiff_9_15_2016_11_52.csv", \
+                              "HyunjiDiff_9_13_2016_12_21.csv", \
+                              "KristenDiff_9_13_2016_13_15.csv", \
+                              "NickDiff_9_15_2016_12_5.csv", \
+                              "Rebeka_Diff_9_9_2016_12_20.csv", \
+                              "Sara_Diff_9_9_2016_12_15.csv"]
+
+trackit_fnames_child_0dis = ["A232-0dis.csv", \
+                             "A233-0dis.csv", \
+                             "A239-0dis.csv", \
+                             "A240-0dis.csv", \
+                             "A2410-0dis.csv", \
+                             "A242-0dis.csv", \
+                             "A243-0dis.csv", \
+                             "A244-0dis.csv", \
+                             "A245-0dis.csv", \
+                             "A247-0dis.csv"]
+
+eyetrack_fnames_child_0dis = ["A232-0dis_3_29_2016_10_21.csv", \
+                              "A233-0dis_3_29_2016_10_13.csv", \
+                              "A239-0dis_3_29_2016_9_15.csv", \
+                              "A240-0dis_3_29_2016_9_34.csv", \
+                              "A241-0dis_3_22_2016_8_57.csv", \
+                              "A242-0dis_3_22_2016_9_21.csv", \
+                              "A243-0dis_3_22_2016_10_7.csv", \
+                              "A244-0dis_3_22_2016_9_34.csv", \
+                              "A245-0dis_3_29_2016_9_3.csv", \
+                              "A247-0dis_3_22_2016_10_24.csv"]
+
+trackit_fnames_child_same = ["A232Same.csv", \
+                             "A233Same.csv", \
+                             "A239Same.csv", \
+                             "A240Same.csv", \
+                             "A241Same.csv", \
+                             "A242Same.csv", \
+                             "A243Same.csv", \
+                             "A245Same.csv", \
+                             "A247Same.csv"]
+
+eyetrack_fnames_child_same = ["A232Same_3_29_2016_10_26.csv", \
+                              "A233Same_3_29_2016_10_10.csv", \
+                              "A239Same_3_29_2016_9_19.csv", \
+                              "A240Same_3_29_2016_9_28.csv", \
+                              "A241Same_3_22_2016_9_1.csv", \
+                              "A242Same_3_22_2016_9_15.csv", \
+                              "A243Same_3_22_2016_10_13.csv", \
+                              "A244Same_3_22_2016_9_27.csv", \
+                              "A245same_3_29_2016_9_5.csv", \
+                              "A247Same_3_22_2016_10_21.csv"]
+
+trackit_fnames_child_diff = ["A232Diff.csv", \
+                             "A233Diff.csv", \
+                             "A239Diff.csv", \
+                             "A240Diff.csv", \
+                             "A241Diff.csv", \
+                             "A242Diff.csv", \
+                             "A243Diff.csv", \
+                             "A244Diff.csv", \
+                             "A245Diff.csv", \
+                             "A247Diff.csv"]
+
+eyetrack_fnames_child_diff = ["A232Diff_3_29_2016_10_23.csv", \
+                              "A233Diff_3_29_2016_10_5.csv", \
+                              "A239Diff_3_29_2016_9_21.csv", \
+                              "A240Diff_3_29_2016_9_32.csv", \
+                              "A241Diff_3_22_2016_9_4.csv", \
+                              "A242Diff_3_22_2016_9_19.csv", \
+                              "A243Diff_3_22_2016_10_10.csv", \
+                              "A244Diff_3_22_2016_9_31.csv", \
+                              "A245Diff_3_29_2016_9_0.csv", \
+                              "A247Diff_3_22_2016_10_27.csv"]
+
+print len(trackit_fnames_adult_0dis)
+print len(eyetrack_fnames_adult_0dis)
+print len(trackit_fnames_adult_same)
+print len(trackit_fnames_adult_same)
+print len(trackit_fnames_adult_diff)
+print len(eyetrack_fnames_adult_diff)
+print len(trackit_fnames_child_0dis)
+print len(eyetrack_fnames_child_0dis)
+print len(trackit_fnames_child_same)
+print len(eyetrack_fnames_child_same)
+print len(trackit_fnames_child_diff)
+print len(eyetrack_fnames_child_diff)
