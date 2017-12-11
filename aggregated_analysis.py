0a6f27956d513762077f452a5f9057ed418c5ecf
sss1 <sss1@andrew.cmu.edu>
2017-12-11 14:19:38 -0500

    Added plots of data retained at various missing data thresholds.

--- aggregated_analysis.py
+++ aggregated_analysis.py
@@ -41,50 +41,59 @@ data_child_0dis = [preprocess_all(*subject_data) for subject_data in data_child_
 data_child_same = [preprocess_all(*subject_data) for subject_data in data_child_same]
 data_child_diff = [preprocess_all(*subject_data) for subject_data in data_child_diff]
 
-plt.subplot(3, 2, 1)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_0dis for trial_data in subject_data[0]])
-plt.title('Adult, 0 distractors')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
+# plt.subplot(3, 2, 1)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_0dis for trial_data in subject_data[0]])
+# plt.title('Adult, 0 distractors')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.subplot(3, 2, 3)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_same for trial_data in subject_data[0]])
+# plt.title('Adult, All Same')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.subplot(3, 2, 5)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_diff for trial_data in subject_data[0]])
+# plt.title('Adult, All Different')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.subplot(3, 2, 2)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_0dis for trial_data in subject_data[0]])
+# plt.title('Child, 0 distractors')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.subplot(3, 2, 4)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_same for trial_data in subject_data[0]])
+# plt.title('Child, All Same')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.subplot(3, 2, 6)
+# plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_diff for trial_data in subject_data[0]])
+# plt.title('Child, All Different')
+# plt.xlim(0, 1)
+# plt.ylim(0, 50)
+# 
+# plt.gcf().suptitle('Distribution of Proportion of Missing Data per Trial', fontsize = "x-large")
+# plt.show()
 
-plt.subplot(3, 2, 3)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_same for trial_data in subject_data[0]])
-plt.title('Adult, All Same')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
-
-plt.subplot(3, 2, 5)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_adult_diff for trial_data in subject_data[0]])
-plt.title('Adult, All Different')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
-
-plt.subplot(3, 2, 2)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_0dis for trial_data in subject_data[0]])
-plt.title('Child, 0 distractors')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
-
-plt.subplot(3, 2, 4)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_same for trial_data in subject_data[0]])
-plt.title('Child, All Same')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
-
-plt.subplot(3, 2, 6)
-plt.hist([np.mean(np.isnan(trial_data[0,:])) for subject_data in data_child_diff for trial_data in subject_data[0]])
-plt.title('Child, All Different')
-plt.xlim(0, 1)
-plt.ylim(0, 50)
-
-plt.gcf().suptitle('Proportion of Missing Data per Trial', fontsize = "x-large")
+data = data_child_0dis
+num_plots = 10
+x_max = max([len(subject_data[0]) for subject_data in data]) + 1
+for threshold_idx in range(num_plots):
+  threshold = threshold_idx/float(num_plots)
+  print threshold
+  plt.subplot(num_plots, 1, threshold_idx + 1)
+  plt.hist([sum([np.mean(np.isnan(trial_data[0,:])) > threshold for trial_data in subject_data[0]]) for subject_data in data], bins = [x/2.0 for x in range(2*x_max)])
+  plt.xlim(0, x_max)
+  plt.ylim(0, len(data_child_0dis))
+  plt.ylabel(str(threshold))
+plt.gcf().text(0.04, 0.5, 'Threshold', va='center', rotation='vertical')
+plt.gcf().suptitle('Distribution of Trials per Subject\nwith Missing Data Proportion at most \"Thresholding\"\n(0 Distractor Condition)', fontsize = "x-large")
 plt.show()
-
-
-for subject_data in data_child_0dis:
-  trial_means = [np.mean(np.isnan(trial_data[0,:])) for trial_data in subject_data[0]]
-  print np.mean(trial_means)
-
 # # Each MLEs_*_* is a subject X trial X timepoint array of states, taking integer values in [0, num_distractors]
 # def time_test1():
 #   MLEs_adult_same = [[get_trackit_MLE(*trial_data) for trial_data in zip(*subject_data)] for subject_data in data_adult_same]
