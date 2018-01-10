ac059f4ca3caec198b129d29f78a5f85802f958a
sss1 <sss1@andrew.cmu.edu>
2018-01-10 11:24:53 -0500

    Ran cross-validation to find best sigma2 on supervised child data.

--- supervised_analysis.py
+++ supervised_analysis.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+from scipy.stats import norm
 
 from load_data import load_full_subject_data
 import data_paths as dp
 from eyetracking_hmm import get_trackit_MLE
 from util import preprocess_all
 
-data_child_super = [load_full_subject_data(*entry, filter_threshold = 1, is_supervised = True) for entry in zip(dp.trackit_fnames_child_supervised, dp.eyetrack_fnames_child_supervised)]
+outfile = 'supervised_analysis.csv'
 
+# Load and preprocess data
+data_child_super = [load_full_subject_data(*entry, filter_threshold = 1, is_supervised = True) for entry in zip(dp.trackit_fnames_child_supervised, dp.eyetrack_fnames_child_supervised)]
 data_child_super = [preprocess_all(*subject_data) for subject_data in data_child_super]
 
 # Split off true labels from unsupervised data
@@ -40,11 +44,40 @@ flattened_data = [trial_data for subject_data in data_child_super for trial_data
 flattened_labels = np.array([frame_data for subject_data in labels for trial_data in subject_data for frame_data in trial_data], dtype = int)
 
 # Range of variance values to try
-sigma2s = np.logspace(2, 8, num = 25)
+sigma2s = np.logspace(2, 8, num = 49)
 
-for sigma2 in sigma2s:
-  MLEs_child_super = [get_trackit_MLE(*trial_data, sigma2 = sigma2) for trial_data in flattened_data]
-  MLEs_flattened = np.array([frame_data for trial_data in MLEs_child_super for frame_data in trial_data], dtype = int) # flatten MLEs across trials
-  err = (MLEs_flattened[MLEs_flattened != -1] != flattened_labels[MLEs_flattened != -1]).mean() # For now, ignore frames with missing data
-  # print 'Couldn\'t classify ' + str((MLEs_flattened == -1).mean()) + ' fraction of frames due to missing data.'
-  print str(sigma2) + ', ' + str(err)
+# # Since fitting the HMM model takes a while, cache the results in a small CSV file outfile
+# with open(outfile, 'wb') as csvfile:
+#   writer = csv.writer(csvfile, delimiter = ',')
+#   for sigma2 in sigma2s:
+#     MLEs_child_super = [get_trackit_MLE(*trial_data, sigma2 = sigma2) for trial_data in flattened_data]
+#     MLEs_flattened = np.array([frame_data for trial_data in MLEs_child_super for frame_data in trial_data], dtype = int) # flatten MLEs across trials
+#     err = (MLEs_flattened[MLEs_flattened != -1] != flattened_labels[MLEs_flattened != -1]).mean() # For now, ignore frames with missing data
+#     std_err = np.sqrt(err*(1 - err)/len(MLEs_flattened[MLEs_flattened != -1]))
+#     # print 'Couldn\'t classify ' + str((MLEs_flattened == -1).mean()) + ' fraction of frames due to missing data.'
+#     print str(sigma2) + ', ' + str(err) + ', ' + str(std_err)
+#     writer.writerow([sigma2, err, std_err])
+
+# Read results from outfile
+errors = np.zeros(sigma2s.shape)
+std_errs = np.zeros(sigma2s.shape)
+with open(outfile, 'rb') as csvfile:
+  reader = csv.reader(csvfile, delimiter = ',')
+  row_idx = 0
+  for row in reader:
+    errors[row_idx] = float(row[1])
+    std_errs[row_idx] = float(row[2])
+    row_idx += 1
+
+type1_error = 0.05
+bonferroni_type1_error = type1_error/len(std_errs) # Convert error bars to uniform error bands via Bonferroni
+plt.errorbar(np.sqrt(sigma2s), errors, yerr = norm.ppf(1 - bonferroni_type1_error/2) * std_errs, zorder = 2)
+plt.plot(np.sqrt(sigma2s), errors)
+plt.xlabel('Standard Deviation (pixels)', fontsize = 24)
+plt.ylabel('Classification Error', fontsize = 24)
+ax = plt.gca()
+ax.set_xscale("log", nonposx = 'clip')
+plt.ylim((0, 1))
+min_idx = np.argmin(errors)
+plt.scatter(np.sqrt(sigma2s[min_idx]), errors[min_idx], c = 'r', s = 100, zorder = 1)
+plt.show()
