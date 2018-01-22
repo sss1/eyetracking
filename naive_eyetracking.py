10a574cab1e5bae5f7bc1a0f590fed5721bdeda9
sss1 <sss1@andrew.cmu.edu>
2018-01-22 15:16:37 -0500

    Fixed a bug with propogating missing data through the naive model.

--- naive_eyetracking.py
+++ naive_eyetracking.py
@@ -8,8 +8,11 @@ def get_trackit_MLE(eye_track, target, distractors, sigma2 = None):
   all_distances = np.zeros(num_distractors + 1)
 
   for n in range(N):
-    all_distances[0] = np.linalg.norm(eye_track[:, n] - target[:, n])
-    for k in range(num_distractors):
-      all_distances[k + 1] = np.linalg.norm(eye_track[:, n] - distractors[k, :, n])
-    MLE[n] = np.argmin(all_distances)
+    if np.isnan(eye_track[0, n]):
+      MLE[n] = -1
+    else:
+      all_distances[0] = np.linalg.norm(eye_track[:, n] - target[:, n])
+      for k in range(num_distractors):
+        all_distances[k + 1] = np.linalg.norm(eye_track[:, n] - distractors[k, :, n])
+      MLE[n] = np.argmin(all_distances)
   return MLE
