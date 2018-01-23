Converted aggregated analysis to be trialwise, and fixed calculation of standard errors of accuracies to be appropriate for means of proportions (rather than actual proportions).

aggregated_analysis.py
@@ -1,5 +1,6 @@
 from load_data import load_full_subject_data
-from eyetracking_hmm import get_trackit_MLE
+import eyetracking_hmm
+import naive_eyetracking
 import matplotlib.pyplot as plt
 from util import preprocess_all, jagged_to_numpy
 import data_paths as dp
@@ -23,10 +24,10 @@ print 'Child Same: ' + str(np.mean(np.isnan([x for subject_data in data_child_sa
 print 'Child Diff: ' + str(np.mean(np.isnan([x for subject_data in data_child_diff for trial_data in subject_data[0] for x in trial_data[0]])))
 
 # Preprocess data (synchronize TrackIt with eyetracking, and interpolate some missing data
-data_adult_0dis = [preprocess_all(*subject_data) for subject_data in data_adult_0dis]
+# data_adult_0dis = [preprocess_all(*subject_data) for subject_data in data_adult_0dis]
 data_adult_same = [preprocess_all(*subject_data) for subject_data in data_adult_same]
 data_adult_diff = [preprocess_all(*subject_data) for subject_data in data_adult_diff]
-data_child_0dis = [preprocess_all(*subject_data) for subject_data in data_child_0dis]
+# data_child_0dis = [preprocess_all(*subject_data) for subject_data in data_child_0dis]
 data_child_same = [preprocess_all(*subject_data) for subject_data in data_child_same]
 data_child_diff = [preprocess_all(*subject_data) for subject_data in data_child_diff]
 
@@ -93,7 +94,9 @@ data_child_diff = [preprocess_all(*subject_data) for subject_data in data_child_
 # mng.resize(*mng.window.maxsize())
 # plt.show()
 
-sigma2_child = 421.696503429 ** 2 # Value taken from supervised analysis
+# Values taken from supervised analysis; TODO: Update these once we've collected more supervised data
+sigma2_adult = 562.34132519 ** 2
+sigma2_child = 562.34132519 ** 2
 
 print '\nMissing data after interpolation:'
 print 'Adult Same: ' + str(np.mean(np.isnan([x for subject_data in data_adult_same for trial_data in subject_data[0] for x in trial_data[0]])))
@@ -103,11 +106,10 @@ print 'Child Diff: ' + str(np.mean(np.isnan([x for subject_data in data_child_di
 
 # # Apply HMM analysis to each dataset
 # # Each MLEs_*_* is a subject X trial X timepoint array of states, taking integer values in [0, num_distractors]
-# # TODO: Once we've added an inattentive state, run MLEs for 0dis condition too
-# MLEs_adult_same = [[get_trackit_MLE(*trial_data) for trial_data in zip(*subject_data)] for subject_data in data_adult_same]
-# MLEs_adult_diff = [[get_trackit_MLE(*trial_data) for trial_data in zip(*subject_data)] for subject_data in data_adult_diff]
-# MLEs_child_same = [[get_trackit_MLE(*trial_data) for trial_data in zip(*subject_data)] for subject_data in data_child_same]
-# MLEs_child_diff = [[get_trackit_MLE(*trial_data) for trial_data in zip(*subject_data)] for subject_data in data_child_diff]
+# MLEs_adult_same = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_adult) for trial_data in zip(*subject_data)] for subject_data in data_adult_same]
+# MLEs_adult_diff = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_adult) for trial_data in zip(*subject_data)] for subject_data in data_adult_diff]
+# MLEs_child_same = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_child) for trial_data in zip(*subject_data)] for subject_data in data_child_same]
+# MLEs_child_diff = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_child) for trial_data in zip(*subject_data)] for subject_data in data_child_diff]
 # # Since HMM analysis is slow, cache the results
 # np.savez(dp.root + 'tmp/' + 'MLEs_all.npz', \
 #          MLEs_adult_same = MLEs_adult_same, \
@@ -122,20 +124,49 @@ MLEs_adult_diff = MLEs_cache_file['MLEs_adult_diff']
 MLEs_child_same = MLEs_cache_file['MLEs_child_same']
 MLEs_child_diff = MLEs_cache_file['MLEs_child_diff']
 
+# Calculate TrackIt performance (proportion of frames on target) for each trial
+trial_accuracies_adult_same = [np.mean(trial_data == 0) for subject_data in MLEs_adult_same for trial_data in subject_data]
+trial_accuracies_adult_diff = [np.mean(trial_data == 0) for subject_data in MLEs_adult_diff for trial_data in subject_data]
+trial_accuracies_child_same = [np.mean(trial_data == 0) for subject_data in MLEs_child_same for trial_data in subject_data]
+trial_accuracies_child_diff = [np.mean(trial_data == 0) for subject_data in MLEs_child_diff for trial_data in subject_data]
+# Calculate mean accuracy across trials
+accuracy_adult_same = np.mean(trial_accuracies_adult_same)
+accuracy_adult_diff = np.mean(trial_accuracies_adult_diff)
+accuracy_child_same = np.mean(trial_accuracies_child_same)
+accuracy_child_diff = np.mean(trial_accuracies_child_diff)
+# Calculate 95% confidence radius around each mean accuracy
+CI_adult_same = 1.96 * np.std(trial_accuracies_adult_same) / np.sqrt(len(trial_accuracies_adult_same))
+CI_adult_diff = 1.96 * np.std(trial_accuracies_adult_diff) / np.sqrt(len(trial_accuracies_adult_diff))
+CI_child_same = 1.96 * np.std(trial_accuracies_child_same) / np.sqrt(len(trial_accuracies_child_same))
+CI_child_diff = 1.96 * np.std(trial_accuracies_child_diff) / np.sqrt(len(trial_accuracies_child_diff))
 print '\nProportion of time (total frames) on target:'
-print 'Adult Same: ' + str(np.mean([x == 0 for subject_data in MLEs_adult_same for trial_data in subject_data for x in trial_data]))
-print 'Adult Diff: ' + str(np.mean([x == 0 for subject_data in MLEs_adult_diff for trial_data in subject_data for x in trial_data]))
-print 'Child Same: ' + str(np.mean([x == 0 for subject_data in MLEs_child_same for trial_data in subject_data for x in trial_data]))
-print 'Child Diff: ' + str(np.mean([x == 0 for subject_data in MLEs_child_diff for trial_data in subject_data for x in trial_data]))
+print 'Adult Same: ' + str(accuracy_adult_same) + ' +/- ' + str(CI_adult_same)
+print 'Adult Diff: ' + str(accuracy_adult_diff) + ' +/- ' + str(CI_adult_diff)
+print 'Child Same: ' + str(accuracy_child_same) + ' +/- ' + str(CI_child_same)
+print 'Child Diff: ' + str(accuracy_child_diff) + ' +/- ' + str(CI_child_diff)
 
-print '\nProportion of time (non-missing or interpolated frames) on target:'
-print 'Adult Same: ' + str(np.mean([x == 0 for subject_data in MLEs_adult_same for trial_data in subject_data for x in trial_data[trial_data > -1]]))
-print 'Adult Diff: ' + str(np.mean([x == 0 for subject_data in MLEs_adult_diff for trial_data in subject_data for x in trial_data[trial_data > -1]]))
-print 'Child Same: ' + str(np.mean([x == 0 for subject_data in MLEs_child_same for trial_data in subject_data for x in trial_data[trial_data > -1]]))
-print 'Child Diff: ' + str(np.mean([x == 0 for subject_data in MLEs_child_diff for trial_data in subject_data for x in trial_data[trial_data > -1]]))
+# Repeat above block, but using only frames with non-missing data
+# Calculate TrackIt performance (proportion of non-missing frames on target) for each trial
+trial_accuracies_adult_same = [np.mean(trial_data[trial_data > -1] == 0) for subject_data in MLEs_adult_same for trial_data in subject_data]
+trial_accuracies_adult_diff = [np.mean(trial_data[trial_data > -1] == 0) for subject_data in MLEs_adult_diff for trial_data in subject_data]
+trial_accuracies_child_same = [np.mean(trial_data[trial_data > -1] == 0) for subject_data in MLEs_child_same for trial_data in subject_data]
+trial_accuracies_child_diff = [np.mean(trial_data[trial_data > -1] == 0) for subject_data in MLEs_child_diff for trial_data in subject_data]
+# Calculate mean accuracy across trials
+accuracy_adult_same = np.nanmean(trial_accuracies_adult_same)
+accuracy_adult_diff = np.nanmean(trial_accuracies_adult_diff)
+accuracy_child_same = np.nanmean(trial_accuracies_child_same)
+accuracy_child_diff = np.nanmean(trial_accuracies_child_diff)
+# Calculate 95% confidence radius around each mean accuracy
+CI_adult_same = 1.96 * np.nanstd(trial_accuracies_adult_same) / np.sqrt(len(trial_accuracies_adult_same))
+CI_adult_diff = 1.96 * np.nanstd(trial_accuracies_adult_diff) / np.sqrt(len(trial_accuracies_adult_diff))
+CI_child_same = 1.96 * np.nanstd(trial_accuracies_child_same) / np.sqrt(len(trial_accuracies_child_same))
+CI_child_diff = 1.96 * np.nanstd(trial_accuracies_child_diff) / np.sqrt(len(trial_accuracies_child_diff))
+print '\nProportion of time (non-missing frames) on target:'
+print 'Adult Same: ' + str(accuracy_adult_same) + ' +/- ' + str(CI_adult_same)
+print 'Adult Diff: ' + str(accuracy_adult_diff) + ' +/- ' + str(CI_adult_diff)
+print 'Child Same: ' + str(accuracy_child_same) + ' +/- ' + str(CI_child_same)
+print 'Child Diff: ' + str(accuracy_child_diff) + ' +/- ' + str(CI_child_diff)
 
-
-# No need to continue distinguishing trials from different subjects; replace state with 0 if on target, 1 else
 # Plot proportion of trials on target over trial time
 plt.figure(1)
 aligned_adult_same = jagged_to_numpy([[float(x == 0) for x in trial_data] for subject_data in MLEs_adult_same for trial_data in subject_data])
@@ -149,7 +180,6 @@ plt.plot(np.nanmean(aligned_child_same, axis = 0))
 plt.plot(np.nanmean(aligned_child_diff, axis = 0))
 
 plt.legend(['Adult Same', 'Adult Diff', 'Child Same', 'Child Diff'])
-
 
 # For each dataset, plot a vertical line indicating the length of the shortest trial
 min_trial_len_adult_same = min([len(trial_data) for subject_data in MLEs_adult_same for trial_data in subject_data])

supervised_analysis.py
@@ -78,9 +78,10 @@ with open(adult_cachefile, 'wb') as csvfile:
     MLEs_adult_super = [eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2) for trial_data in flattened_adult_data]
   
     # Calculate accuracy for each trial
-    trial_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_adult_super, flattened_adult_labels)]
-    adult_accuracy = np.mean(trial_accuracies)
-    adult_accuracy_std_err = np.sqrt(adult_accuracy*(1 - adult_accuracy)/len(trial_accuracies))
+    adult_trial_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_adult_super, flattened_adult_labels)]
+    # Calculate mean and standard error across trials
+    adult_accuracy = np.mean(adult_trial_accuracies)
+    adult_accuracy_std_err = np.std(adult_trial_accuracies) / np.sqrt(len(adult_trial_accuracies))
     writer.writerow([sigma2, adult_accuracy, adult_accuracy_std_err])
     print str(sigma2) + ', ' + str(adult_accuracy) + ', ' + str(adult_accuracy_std_err)
     sys.stdout.flush()
@@ -92,9 +93,10 @@ with open(child_cachefile, 'wb') as csvfile:
     MLEs_child_super = [eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2) for trial_data in flattened_child_data]
   
     # Calculate accuracy for each trial
-    trial_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_child_super, flattened_child_labels)]
-    child_accuracy = np.mean(trial_accuracies)
-    child_accuracy_std_err = np.sqrt(child_accuracy*(1 - child_accuracy)/len(trial_accuracies))
+    child_trial_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_child_super, flattened_child_labels)]
+    # Calculate mean and standard error across trials
+    child_accuracy = np.mean(child_trial_accuracies)
+    child_accuracy_std_err = np.std(child_trial_accuracies) / np.sqrt(len(child_trial_accuracies))
     writer.writerow([sigma2, child_accuracy, child_accuracy_std_err])
     print str(sigma2) + ', ' + str(child_accuracy) + ', ' + str(child_accuracy_std_err)
     sys.stdout.flush()
@@ -103,7 +105,7 @@ with open(child_cachefile, 'wb') as csvfile:
 MLEs_adult_super = [naive_eyetracking.get_trackit_MLE(*trial_data) for trial_data in flattened_adult_data]
 adult_naive_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_adult_super, flattened_adult_labels)]
 adult_naive_accuracy = np.mean(adult_naive_accuracies)
-adult_naive_accuracy_std_err = np.sqrt(adult_naive_accuracy*(1 - adult_naive_accuracy) / len(adult_naive_accuracies))
+adult_naive_accuracy_std_err = np.std(adult_naive_accuracies) / np.sqrt(len(adult_naive_accuracies))
 print '\nNaive model accuracy on adults is ' + str(adult_naive_accuracy) + \
       ', with standard error ' + str(adult_naive_accuracy_std_err) + \
       ', based on ' + str(len(adult_naive_accuracies)) + ' trials.'
@@ -112,7 +114,7 @@ print '\nNaive model accuracy on adults is ' + str(adult_naive_accuracy) + \
 MLEs_child_super = [naive_eyetracking.get_trackit_MLE(*trial_data) for trial_data in flattened_child_data]
 child_naive_accuracies = [(estimate == truth).mean() for (estimate, truth) in zip(MLEs_child_super, flattened_child_labels)]
 child_naive_accuracy = np.mean(child_naive_accuracies)
-child_naive_accuracy_std_err = np.sqrt(child_naive_accuracy*(1 - child_naive_accuracy) / len(child_naive_accuracies))
+child_naive_accuracy_std_err = np.std(child_naive_accuracies) / np.sqrt(len(child_naive_accuracies))
 print '\nNaive model accuracy on children is ' + str(child_naive_accuracy) + \
       ', with standard error ' + str(child_naive_accuracy_std_err) + \
       ', based on ' + str(len(child_naive_accuracies)) + ' trials.'