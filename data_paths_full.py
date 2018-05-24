109d463d079cb134e3081b96660b32753c97052b
sss1 <sss1@andrew.cmu.edu>
2018-05-24 11:46:20 -0400

    Added analysis and data paths for shrinky experiment.

---
+++ data_paths_full.py
@@ -0,0 +1,159 @@
+# root = "/home/sss1/Desktop/projects/eyetracking/data/" # Office desktop
+# root = "/home/painkiller/Desktop/academic/projects/trackit/eyetracking/data/" # Laptop
+root = "/home/sss1/Desktop/academic/projects/eyetracking/data/" # Home desktop
+
+eyetrack_fnames_shrinky = ["AF10shrinky_5_14_2018_9_48.csv", \
+                           "AF11shrinky_5_14_2018_10_20.csv", \
+                           "AF2shrinky_5_14_2018_10_9.csv", \
+                           "AF5shrinky_5_7_2018_10_43.csv", \
+                           "AF7shrinky_5_7_2018_9_45.csv", \
+                           "AF8shrinky_5_7_2018_10_1.csv", \
+                           "B33shrinky_5_9_2018_10_9.csv", \
+                           "B37shrinky_5_2_2018_10_20.csv", \
+                           "B38shrinky_5_9_2018_10_56.csv", \
+                           "B40shrinky_5_2_2018_10_42.csv", \
+                           "B42shrinky_5_2_2018_10_58.csv", \
+                           "D5shrinky_5_1_2018_10_31.csv", \
+                           "D6shrinky_5_3_2018_9_42.csv", \
+                           "E22shrinky_4_30_2018_9_46.csv", \
+                           "E29shrinky_4_30_2018_10_0.csv", \
+                           "E3shrinky_4_27_2018_10_23.csv", \
+                           "E51shrinky_4_27_2018_9_56.csv", \
+                           "E52shrinky_4_30_2018_10_26.csv", \
+                           "M114shrinky_5_4_2018_13_15.csv", \
+                           "M115shrinky_5_4_2018_13_35.csv", \
+                           "M117shrinky_5_11_2018_13_3.csv", \
+                           "M138shrinky_5_4_2018_10_46.csv", \
+                           "M139shrinky_5_11_2018_10_43.csv", \
+                           "M140shrinky_5_4_2018_9_51.csv", \
+                           "M141shrinky_5_11_2018_10_4.csv", \
+                           "M143shrinky_5_11_2018_9_55.csv", \
+                           "M144shrinky_5_11_2018_11_2.csv", \
+                           "M146shrinky_5_11_2018_10_25.csv", \
+                           "M147shrinky_5_4_2018_10_59.csv", \
+                           "R11shrinky_4_24_2018_9_28.csv", \
+                           "R12shrinky_4_26_2018_9_33.csv", \
+                           "R16shrinky_4_24_2018_10_15.csv", \
+                           "R17shrinky_4_24_2018_10_34.csv", \
+                           "R34shrinky_4_26_2018_10_10.csv", \
+                           "R35shrinky_5_8_2018_10_8.csv", \
+                           "R37shrinky_5_8_2018_9_26.csv"]
+
+trackit_fnames_shrinky = ["AF10shrinky.csv", \
+                          "AF11shrinky.csv", \
+                          "AF2shrinky.csv", \
+                          "AF5shrinky.csv", \
+                          "AF7shrinky.csv", \
+                          "AF8shrinky.csv", \
+                          # "B31shrinky.csv", \
+                          "B33shrinky.csv", \
+                          "B37shrinky.csv", \
+                          "B38shrinky.csv", \
+                          "B40shrinky.csv", \
+                          "B42shrinky.csv", \
+                          "D5shrinky.csv", \
+                          "D6shrinky.csv", \
+                          "E22shrinky.csv", \
+                          "E29shrinky.csv", \
+                          "E3shrinky.csv", \
+                          "E51shrinky.csv", \
+                          "E52shrinky.csv", \
+                          "M114shrinky.csv", \
+                          "M115shrinky.csv", \
+                          "M117shrinky.csv", \
+                          # "M137shrinky.csv", \
+                          "M138shrinky.csv", \
+                          "M139shrinky.csv", \
+                          "M140shrinky.csv", \
+                          "M141shrinky.csv", \
+                          "M143shrinky.csv", \
+                          "M144shrinky.csv", \
+                          "M146shrinky.csv", \
+                          "M147shrinky.csv", \
+                          "R11shrinky.csv", \
+                          "R12shrinky.csv", \
+                          "R16shrinky.csv", \
+                          "R17shrinky.csv", \
+                          "R34shrinky.csv", \
+                          "R35shrinky.csv", \
+                          "R37shrinky.csv"]
+
+eyetrack_fnames_noshrinky = ["AF10noshrinky_5_7_2018_10_57.csv", \
+                             "AF11noshrinky_5_7_2018_10_17.csv", \
+                             "AF2noshrinky_5_7_2018_10_30.csv", \
+                             "AF7noshrinky_5_14_2018_9_36.csv", \
+                             "AF8noshrinky_5_14_2018_9_21.csv", \
+                             "B31noshrinky_5_9_2018_10_40.csv", \
+                             "B33noshrinky_5_2_2018_9_45.csv", \
+                             "B37noshrinky_5_9_2018_10_26.csv", \
+                             "B38noshrinky_5_2_2018_11_16.csv", \
+                             "B40noshrinky_5_23_2018_13_43.csv", \
+                             "B42noshrinky_5_23_2018_13_58.csv", \
+                             "D5noshrinky_5_3_2018_9_24.csv", \
+                             "D6noshrinky_5_1_2018_10_11.csv", \
+                             "E22noshrinky_4_27_2018_9_41.csv", \
+                             "E29noshrinky_4_27_2018_10_47.csv", \
+                             "E3noshrinky_4_30_2018_10_13.csv", \
+                             "E51noshrinky_4_30_2018_10_38.csv", \
+                             "E52noshrinky_4_27_2018_10_39.csv", \
+                             "M114noshrinky_5_11_2018_13_15.csv", \
+                             "M115noshrinky_5_11_2018_13_27.csv", \
+                             "M117noshrinky_5_4_2018_12_51.csv", \
+                             "M137noshrinky_5_11_2018_10_33.csv", \
+                             "M138noshrinky_5_11_2018_10_53.csv", \
+                             "M139noshrinky_5_4_2018_10_33.csv", \
+                             "M140noshrinky_5_11_2018_10_16.csv", \
+                             "M141noshrinky_5_4_2018_9_28.csv", \
+                             "M142noshrinky_5_4_2018_13_48.csv", \
+                             "M143noshrinky_5_4_2018_9_40.csv", \
+                             "M144noshrinky_5_4_2018_11_9.csv", \
+                             "M146noshrinky_5_4_2018_10_16.csv", \
+                             "M147noshrinky_5_11_2018_11_10.csv", \
+                             "R12noshrinky_4_24_2018_9_50.csv", \
+                             "R17noshrinky_5_8_2018_9_57.csv", \
+                             "R34noshrinky_5_8_2018_9_44.csv", \
+                             "R35noshrinky_4_26_2018_10_27.csv", \
+                             "R37noshrinky_4_24_2018_10_1.csv"]
+
+trackit_fnames_noshrinky = ["AF10noshrinky.csv", \
+                            "AF11noshrinky.csv", \
+                            "AF2noshrinky.csv", \
+                            "AF7noshrinky.csv", \
+                            "AF8noshrinky.csv", \
+                            "B31noshrinky.csv", \
+                            "B33noshrinky.csv", \
+                            "B37noshrinky.csv", \
+                            "B38noshrinky.csv", \
+                            "B40noshrinky.csv", \
+                            "B42noshrinky.csv", \
+                            "D5noshrinky.csv", \
+                            "D6noshrinky.csv", \
+                            "E22noshrinky.csv", \
+                            "E29noshrinky.csv", \
+                            "E3noshrinky.csv", \
+                            "E51noshrinky.csv", \
+                            "E52noshrinky.csv", \
+                            "M114noshrinky.csv", \
+                            "M115noshrinky.csv", \
+                            "M117noshrinky.csv", \
+                            "M137noshrinky.csv", \
+                            "M138noshrinky.csv", \
+                            "M139noshrinky.csv", \
+                            "M140noshrinky.csv", \
+                            "M141noshrinky.csv", \
+                            "M142noshrinky.csv", \
+                            "M143noshrinky.csv", \
+                            "M144noshrinky.csv", \
+                            "M146noshrinky.csv", \
+                            "M147noshrinky.csv", \
+                            # "R11noshrinky.csv", \
+                            "R12noshrinky.csv", \
+                            "R17noshrinky.csv", \
+                            "R34noshrinky.csv", \
+                            "R35noshrinky.csv", \
+                            "R37noshrinky.csv"]
+
+trackit_fnames_shrinky  = [root + "shrinky/TrackItOutput/shrinky/" + fname for fname in trackit_fnames_shrinky]
+eyetrack_fnames_shrinky = [root + "shrinky/EyeTracker/shrinky/" + fname for fname in eyetrack_fnames_shrinky]
+trackit_fnames_noshrinky  = [root + "shrinky/TrackItOutput/noshrinky/" + fname for fname in trackit_fnames_noshrinky]
+eyetrack_fnames_noshrinky = [root + "shrinky/EyeTracker/noshrinky/" + fname for fname in eyetrack_fnames_noshrinky]
---
+++ shrinky_analysis.py
@@ -0,0 +1,101 @@
+from load_data import load_full_subject_data
+import eyetracking_hmm
+import naive_eyetracking
+import matplotlib.pyplot as plt
+from util import preprocess_all, jagged_to_numpy
+import data_paths_full as dp # Large unsupervised (shrinky) dataset
+import numpy as np
+
+# load datasets
+data_shrinky = [load_full_subject_data(*entry, filter_threshold = 1) for entry in zip(dp.trackit_fnames_shrinky, dp.eyetrack_fnames_shrinky)]
+data_noshrinky = [load_full_subject_data(*entry, filter_threshold = 1) for entry in zip(dp.trackit_fnames_noshrinky, dp.eyetrack_fnames_noshrinky)]
+
+print '\nMissing data before interpolation and discarding:'
+print 'Shrinky: ' + str(np.mean(np.isnan([x for subject_data in data_shrinky for trial_data in subject_data[0] for x in trial_data[0]])))
+print 'No Shrinky: ' + str(np.mean(np.isnan([x for subject_data in data_noshrinky for trial_data in subject_data[0] for x in trial_data[0]])))
+
+# Preprocess data (synchronize TrackIt with eyetracking, and interpolate some missing data, and discard trials/subjects
+# with too much missing data
+trial_discard_threshold = 0.5 # 0 discards trials with *any* missing data; 1 keeps all trials
+subject_discard_threshold = 0.5 # 0 discards subjects with *any* missing trials; 1 keeps all subjects
+data_shrinky = [preprocess_all(*subject_data, \
+                               trial_discard_threshold = trial_discard_threshold, \
+                               subject_discard_threshold = subject_discard_threshold) \
+                for subject_data in data_shrinky]
+data_shrinky = [subject_data for subject_data in data_shrinky if subject_data[0] is not None]
+data_noshrinky = [preprocess_all(*subject_data, \
+                               trial_discard_threshold = trial_discard_threshold, \
+                               subject_discard_threshold = subject_discard_threshold) \
+                for subject_data in data_noshrinky]
+data_noshrinky = [subject_data for subject_data in data_noshrinky if subject_data[0] is not None]
+
+print '\nMissing data after interpolation and discarding:'
+print 'Shrinky: ' + str(np.mean(np.isnan([x for subject_data in data_shrinky for trial_data in subject_data[0] for x in trial_data[0]])))
+print 'No Shrinky: ' + str(np.mean(np.isnan([x for subject_data in data_noshrinky for trial_data in subject_data[0] for x in trial_data[0]])))
+
+# Values taken from supervised results in CogSci 18 paper
+sigma2_adult = 490 ** 2
+sigma2_child = 870 ** 2
+
+# # Apply HMM analysis to each dataset
+# # Each MLEs_*_* is a subject X trial X timepoint array of states, taking integer values in [0, num_distractors]
+# MLEs_shrinky = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_adult) for trial_data in zip(*subject_data)] for subject_data in data_shrinky]
+# MLEs_noshrinky = [[eyetracking_hmm.get_trackit_MLE(*trial_data, sigma2 = sigma2_adult) for trial_data in zip(*subject_data)] for subject_data in data_noshrinky]
+# # Since HMM analysis is slow, cache the classifications (estimating state sequences)
+# np.savez(dp.root + 'cache/' + 'MLEs_shrinky_analysis.npz', \
+#          MLEs_shrinky = MLEs_shrinky, \
+#          MLEs_noshrinky = MLEs_noshrinky)
+
+# Load HMM analysis classifications from cache file
+MLEs_cache_file = np.load(dp.root + 'cache/' + 'MLEs_shrinky_analysis.npz')
+MLEs_shrinky = MLEs_cache_file['MLEs_shrinky']
+MLEs_noshrinky = MLEs_cache_file['MLEs_noshrinky']
+
+
+# Plot proportion of (all) trials on target over trial time
+plt.figure(1)
+aligned_shrinky = jagged_to_numpy([[float(x == 0) for x in trial_data] for subject_data in MLEs_shrinky for trial_data in subject_data])
+aligned_noshrinky = jagged_to_numpy([[float(x == 0) for x in trial_data] for subject_data in MLEs_noshrinky for trial_data in subject_data])
+plt.plot(np.nanmean(aligned_shrinky, axis = 0))
+plt.plot(np.nanmean(aligned_noshrinky, axis = 0))
+plt.legend(['Shrinky', 'No Shrinky'])
+# For each dataset, plot a vertical line indicating the length of the shortest trial
+min_trial_len_shrinky = min([len(trial_data) for subject_data in MLEs_shrinky for trial_data in subject_data])
+min_trial_len_noshrinky = min([len(trial_data) for subject_data in MLEs_noshrinky for trial_data in subject_data])
+plt.gca().set_color_cycle(None)
+plt.plot([min_trial_len_shrinky, min_trial_len_shrinky], [0, 1])
+plt.plot([min_trial_len_noshrinky, min_trial_len_noshrinky], [0, 1])
+plt.xlabel('Time (frames, at 60Hz)')
+plt.ylabel('Fraction of all trials on target')
+
+# Plot proportion of (non-missing or interpolated) trials on target over trial time
+plt.figure(2)
+def on_target_or_nan(x): # replace missing data (encoded as -1) with nan
+  if x == -1:
+    return float("nan")
+  return x == 0
+aligned_shrinky = jagged_to_numpy([[on_target_or_nan(x) for x in trial_data] for subject_data in MLEs_shrinky for trial_data in subject_data])
+aligned_noshrinky = jagged_to_numpy([[on_target_or_nan(x) for x in trial_data] for subject_data in MLEs_noshrinky for trial_data in subject_data])
+plt.plot(np.nanmean(aligned_shrinky, axis = 0))
+plt.plot(np.nanmean(aligned_noshrinky, axis = 0))
+plt.legend(['Shrinky', 'No Shrinky'])
+plt.gca().set_color_cycle(None)
+plt.plot([min_trial_len_shrinky, min_trial_len_shrinky], [0, 1])
+plt.plot([min_trial_len_noshrinky, min_trial_len_noshrinky], [0, 1])
+plt.xlabel('Time (frames, at 60Hz)')
+plt.ylabel('Fraction of non-missing or interpolated trials on target')
+
+# Plot proportion of missing data over trial time
+plt.figure(3)
+aligned_shrinky = jagged_to_numpy([[float(x == -1) for x in trial_data] for subject_data in MLEs_shrinky for trial_data in subject_data])
+aligned_noshrinky = jagged_to_numpy([[float(x == -1) for x in trial_data] for subject_data in MLEs_noshrinky for trial_data in subject_data])
+plt.plot(np.nanmean(aligned_shrinky, axis = 0))
+plt.plot(np.nanmean(aligned_noshrinky, axis = 0))
+plt.legend(['Shrinky', 'No Shrinky'])
+plt.gca().set_color_cycle(None)
+plt.plot([min_trial_len_shrinky, min_trial_len_shrinky], [0, 1])
+plt.plot([min_trial_len_noshrinky, min_trial_len_noshrinky], [0, 1])
+plt.xlabel('Trial Time (frames, at 60Hz)')
+plt.ylabel('Fraction of trials missing data')
+
+plt.show()
